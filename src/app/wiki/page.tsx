e5884f2e3ea174a6e42da24601966ff72051c078
Ray <lokto.kwan@gmail.com>
2025-04-29 05:47:24 -0400

    populate wiki page

--- src/app/wiki/page.tsx
+++ src/app/wiki/page.tsx
@@ -13,39 +13,43 @@ import {
 
 import Link from "next/link";
 import React from "react";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { pseudocode as aStarPseudocode } from "@/algorithms/a_star";
+import { tomorrow } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 const searchAlgorithms = [
-  {
-    name: "Linear Search",
-    description:
-      "Checks each element one by one until the target is found or the list ends.",
-  },
-  {
-    name: "Binary Search",
-    description:
-      "Efficiently searches sorted lists by repeatedly halving the search space.",
-  },
-  {
-    name: "Depth-First Search (DFS)",
-    description:
-      "Explores as far as possible along each branch before backtracking.",
-  },
-  {
-    name: "Breadth-First Search (BFS)",
-    description:
-      "Explores all nodes at the current depth before moving to the next level.",
-  },
+  // {
+  //   name: "Linear Search",
+  //   description:
+  //     "Checks each element one by one until the target is found or the list ends.",
+  // },
+  // {
+  //   name: "Binary Search",
+  //   description:
+  //     "Efficiently searches sorted lists by repeatedly halving the search space.",
+  // },
+  // {
+  //   name: "Depth-First Search (DFS)",
+  //   description:
+  //     "Explores as far as possible along each branch before backtracking.",
+  // },
+  // {
+  //   name: "Breadth-First Search (BFS)",
+  //   description:
+  //     "Explores all nodes at the current depth before moving to the next level.",
+  // },
   {
     name: "A* Search",
     key: "a-star",
     description:
       "Finds the shortest path using heuristics to guide the search efficiently.",
+    psudocode: aStarPseudocode,
   },
 ];
 
 export default function SearchAlgorithmsPage() {
   return (
-    <Box sx={{ px: 2, py: 6, maxWidth: "800px", mx: "auto" }}>
+    <Box sx={{ px: 2, py: 6, mx: "auto" }}>
       <Typography variant="h3" component="h1" align="center" gutterBottom>
         Search Algorithms
       </Typography>
@@ -68,6 +72,11 @@ export default function SearchAlgorithmsPage() {
                 <Typography color="text.secondary">
                   {algo.description}
                 </Typography>
+                {algo.psudocode && (
+                  <SyntaxHighlighter language="text" style={tomorrow}>
+                    {algo.psudocode}
+                  </SyntaxHighlighter>
+                )}
               </CardContent>
             </Card>
           </Grid>
