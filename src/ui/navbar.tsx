6e30d08633e94d05edd5b70322f5d1608b675a80
Ray <lokto.kwan@gmail.com>
2025-12-05 04:36:38 -0500

    mobile friendly navbar

--- src/ui/navbar.tsx
+++ src/ui/navbar.tsx
@@ -1,31 +1,90 @@
-// components/NavBar.tsx
-
 "use client";
+
+import React, { useState } from "react";
 
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
 import Link from "next/link";
-import React from "react";
+import { MdMenu } from "react-icons/md";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { useTheme } from "@mui/material/styles";
 
 export default function NavBar() {
-  return (
-    <AppBar position="static" color="primary">
-      <Toolbar>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          <Link href="/" style={{ textDecoration: "none", color: "inherit" }}>
-            Home
-          </Link>
-        </Typography>
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
 
-        <Box>
-          <Button color="inherit" component={Link} href="/wiki">
+  return (
+    <>
+      <AppBar position="static" color="primary">
+        <Toolbar>
+          {/* Left side brand */}
+          <Typography
+            variant="h6"
+            sx={{ flexGrow: 1, cursor: "pointer" }}
+            component={Link}
+            href="/"
+            style={{ textDecoration: "none", color: "inherit" }}
+          >
+            Home
+          </Typography>
+
+          {/* Mobile menu button */}
+          {isMobile ? (
+            <IconButton
+              color="inherit"
+              edge="end"
+              onClick={() => setDrawerOpen(true)}
+            >
+              <MdMenu />
+            </IconButton>
+          ) : (
+            /* Desktop buttons */
+            <Box>
+              <Button color="inherit" component={Link} href="/wiki">
+                Wiki
+              </Button>
+            </Box>
+          )}
+        </Toolbar>
+      </AppBar>
+
+      {/* Drawer for mobile */}
+      <Drawer
+        anchor="right"
+        open={drawerOpen}
+        onClose={() => setDrawerOpen(false)}
+      >
+        <Box
+          sx={{
+            width: 150,
+            p: 2,
+            display: "flex",
+            flexDirection: "column",
+            gap: 2,
+          }}
+        >
+          <Button
+            component={Link}
+            href="/"
+            onClick={() => setDrawerOpen(false)}
+          >
+            Home
+          </Button>
+          <Button
+            component={Link}
+            href="/wiki"
+            onClick={() => setDrawerOpen(false)}
+          >
             Wiki
           </Button>
         </Box>
-      </Toolbar>
-    </AppBar>
+      </Drawer>
+    </>
   );
 }
